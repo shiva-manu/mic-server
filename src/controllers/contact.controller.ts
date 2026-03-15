12c80b8a98d2af6b946e9ebc1d032b9839e34300
shiva-manu <shivamani6964@gmail.com>
2026-03-15 21:29:37 +0530

    Fix: use shared Prisma singleton in contact controller

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -1,7 +1,5 @@
 import type { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '../lib/prisma.js';
 
 export const createContact = async (req: Request, res: Response) => {
     try {
